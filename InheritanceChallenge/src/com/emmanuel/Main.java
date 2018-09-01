7da121f562967e43ccf8c74a6035b88691d1c163
Emmanuel <emmanueljucaban@gmail.com>
2018-09-01 13:46:03 -0700

    Finished composition challenge

--- InheritanceChallenge/src/com/emmanuel/Main.java
+++ InheritanceChallenge/src/com/emmanuel/Main.java
@@ -3,11 +3,24 @@ package com.emmanuel;
 public class Main {
 
   public static void main(String[] args) {
-    // write your code here
-    Car mazda = new Car("Mazda", "small", 4, 4, 6, true);
-    mazda.changeVelocity(1,2);
-    mazda.stop();
+//    // write your code here
+//    Car mazda = new Car("Mazda", "small", 4, 4, 6, true);
+//    mazda.changeVelocity(1,2);
+//    mazda.stop();
+//
+//    Outlander outlander = new Outlander(26);
 
-    Outlander outlander = new Outlander(26);
+
+    Dimensions dimensions = new Dimensions(5, 5, 5);
+    Case theCase = new Case("NZXT", "NZXT", "12v", dimensions);
+
+
+    Monitor monitor = new Monitor("Asus", "Asus", 24, new Resolution(24, 24));
+    Motherboard motherboard = new Motherboard("Razer", "Razer", 6, 2, "No idea");
+
+    PC pc = new PC(theCase, monitor, motherboard);
+
+    // We need to get the case here which is the object that has access to the power button
+    pc.powerUp();
   }
 }
